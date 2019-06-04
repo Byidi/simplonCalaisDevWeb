dc484a1e042cb3e29974eafdc299be0e1f217c7c
Byidi <sajot.vincent@gmail.com>
2019-06-04 16:36:22 +0200

    js cours2 finish

--- js/cours2/js/main.js
+++ js/cours2/js/main.js
@@ -18,3 +18,44 @@ switch (drink) {
     default:
         console.log("Whatever ...");
 }
+
+var array = ['Clement', 'Vianney', 'Marie', 12, false, 1.2, true];
+console.log(array);
+console.log(array.length);
+console.log(array[0]);
+
+console.log("-------------------------");
+for (var i = 0; i < array.length; i++) {
+    console.log(array[i]);
+}
+
+console.log("-------------------------");
+for (var a in array) {
+    console.log(array[a]);
+}
+
+console.log("-------------------------");
+for (a of array) {
+    console.log(a);
+}
+
+console.log("-------------------------");
+array.forEach(function(a){
+    console.log(a);
+});
+
+var max = 100;
+var min = 0;
+var find_min = 999;
+var find_max = 0;
+for (var i = 0; i < 500; i++) {
+    var rdm = Math.floor(Math.random() * (max - min + 1)) + min;
+    if (rdm < find_min){
+        find_min = rdm;
+    }
+    if (rdm > find_max){
+        find_max = rdm;
+    }
+}
+console.log("min : "+find_min);
+console.log("max : "+find_max);
