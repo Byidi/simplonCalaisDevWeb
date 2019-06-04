d4950e98977cd62b53ba7e1c3ab73260dc9a85f5
Byidi <sajot.vincent@gmail.com>
2019-06-04 09:10:42 +0200

    uncomment js ex1 1-5

--- js/base/js/ex1.js
+++ js/base/js/ex1.js
@@ -1,11 +1,11 @@
 //***** 1 *****\\
 
-// var prenom = prompt("Entre ton prénom");
-// alert("Bonjour "+prenom);
+var prenom = prompt("Entre ton prénom");
+alert("Bonjour "+prenom);
 
 //***** 2 *****\\
 
-// console.log("Coucou "+prenom);
+console.log("Coucou "+prenom);
 
 //***** 3 *****\\
 
@@ -17,24 +17,24 @@ div[1].classList.add('colorRed');
 
 //***** 4 *****\\
 
-// var password = "admin"
-// var essai = 0;
-// var pseudo = "";
-// var pass = "";
-//
-// while(pseudo != "admin" || pass != password){
-//     var pseudo = prompt("Pseudo :");
-//     var pass = prompt("Password :");
-//
-//     document.getElementsByClassName("colorRed")[0].innerHTML = "Erreur recommence !!!";
-//     if( essai != password.length || essai != 0){
-//         essai++;
-//         console.log("Indice : "+password.substring(0, essai));
-//     }
-// }
-//
-// document.getElementsByClassName("colorRed")[0].innerHTML = "";
-// document.getElementsByClassName("colorBlue")[0].innerHTML ="Connexion réussi !!!";
+var password = "admin"
+var essai = 0;
+var pseudo = "";
+var pass = "";
+
+while(pseudo != "admin" || pass != password){
+    var pseudo = prompt("Pseudo :");
+    var pass = prompt("Password :");
+
+    document.getElementsByClassName("colorRed")[0].innerHTML = "Erreur recommence !!!";
+    if( essai != password.length || essai != 0){
+        essai++;
+        console.log("Indice : "+password.substring(0, essai));
+    }
+}
+
+document.getElementsByClassName("colorRed")[0].innerHTML = "";
+document.getElementsByClassName("colorBlue")[0].innerHTML ="Connexion réussi !!!";
 
 //***** 5 *****\\
 
@@ -49,26 +49,26 @@ var div5 = document.querySelectorAll(".flex div").forEach(function(div){
 
 //***** 6 *****\\
 
-// var birthday = prompt("Date de naissance : ");
-// var today = prompt("Date du jour : ");
-//
-// var birthdaySplit = birthday.split("/");
-// var todaySplit = today.split("/");
-// var age = parseInt(todaySplit[2],10) - parseInt(birthdaySplit[2],10);
-// if(todaySplit[1] < birthdaySplit[1]){
-//     age--;
-// }else if(todaySplit[1] == birthdaySplit[1] && todaySplit[0] < todaySplit[0]){
-//     age--;
-// }
-//
-//     console.log(birthdaySplit[1] - todaySplit[1]);
-// if(todaySplit[1] == birthdaySplit[1] && todaySplit[0] > birthdaySplit[0]){
-//     alert("Bientôt ton anniversaire !!!");
-// }else if (birthdaySplit[1] - todaySplit[1] <= 0 && birthdaySplit[1] - todaySplit[1] >= -2) {
-//     alert("Un peu de patience.");
-// }else{
-//     alert("T'as encore le temps.");
-// }
+var birthday = prompt("Date de naissance : ");
+var today = prompt("Date du jour : ");
+
+var birthdaySplit = birthday.split("/");
+var todaySplit = today.split("/");
+var age = parseInt(todaySplit[2],10) - parseInt(birthdaySplit[2],10);
+if(todaySplit[1] < birthdaySplit[1]){
+    age--;
+}else if(todaySplit[1] == birthdaySplit[1] && todaySplit[0] < todaySplit[0]){
+    age--;
+}
+
+    console.log(birthdaySplit[1] - todaySplit[1]);
+if(todaySplit[1] == birthdaySplit[1] && todaySplit[0] > birthdaySplit[0]){
+    alert("Bientôt ton anniversaire !!!");
+}else if (birthdaySplit[1] - todaySplit[1] <= 0 && birthdaySplit[1] - todaySplit[1] >= -2) {
+    alert("Un peu de patience.");
+}else{
+    alert("T'as encore le temps.");
+}
 
 //***** 7 *****\\
 
