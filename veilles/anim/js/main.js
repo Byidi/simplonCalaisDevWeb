249b0041f7731b9be5f0860e41f5b8c75469a61d
Byidi <sajot.vincent@gmail.com>
2019-06-11 13:53:34 +0200

    Add sound to veille anim coeur

--- veilles/anim/js/main.js
+++ veilles/anim/js/main.js
@@ -1,19 +1,45 @@
 /*jshint esversion: 9 */
+var timeMax = 30;
 
-setTimeout(stopGame, 5000);
+var gameStart = false;
+var time = timeMax;
+var timer = null;
+
+var move = true;
+var moveStep = 10;
 
 var coeur = document.querySelector(".coeur");
-var body = document.querySelector("body");
+var content = document.querySelector("#content");
 
-for (var i = 0; i < 50; i++) {
+var pew = new Audio('./audio/pew.mp3');
+var boom = new Audio('./audio/boom.mp3');
+var clapclap = new Audio('./audio/clapclap.mp3');
+
+document.getElementById("timer").firstElementChild.innerHTML = time;
+
+setAttr(coeur);
+
+for (var i = 0; i < 100; i++) {
     newCoeur = coeur.cloneNode(true);
-    body.appendChild(newCoeur);
+    content.appendChild(newCoeur);
     setAttr(newCoeur);
 }
 
-var coeur = document.querySelectorAll(".coeur");
-coeur.forEach(function(c){
-    c.addEventListener("click",destroyCoeur);
+content.addEventListener("click", function(){
+    if(gameStart == false){
+        timer = setInterval(updateTimer, 1000);
+        timerPosition = setInterval(updatePosition, 1000);
+        setTimeout(stopGame, timeMax*1000);
+
+        var coeur = document.querySelectorAll(".coeur");
+        coeur.forEach(function(c){
+            c.addEventListener("click",destroyCoeur, {capture:false});
+        });
+
+        gameStart = true;
+    }else{
+        pew.play();
+    }
 });
 
 function setAttr(node){
@@ -47,12 +73,14 @@ function randomAttr(){
 
 function destroyCoeur(node){
     console.log(node);
-    node.target.style.width = "500px";
-    node.target.style.height = "500px";
-    node.target.style.opacity = "0";
-    node.target.style.zIndex = "1";
+    boom.play();
+    let coeur = node.target.closest(".coeur");
+    coeur.style.width = "500px";
+    coeur.style.height = "500px";
+    coeur.style.opacity = "0";
+    coeur.style.zIndex = "1";
     incScore();
-    setTimeout(removeNode, 1000, node.target);
+    setTimeout(removeNode, 1000, coeur);
 }
 
 function removeNode(node){
@@ -60,16 +88,60 @@ function removeNode(node){
 }
 
 function incScore(){
-    var scoreStr = document.querySelector("#score").innerHTML;
+    var scoreStr = document.querySelector("#score").firstElementChild.innerHTML;
     var score = parseInt(scoreStr, 10);
     score++;
-    document.querySelector("#score").innerHTML = score;
+    document.querySelector("#score").firstElementChild.innerHTML = score;
+}
+
+function updatePosition(){
+    if (move == true){
+        let coeurs = document.querySelectorAll(".coeur");
+        coeurs.forEach(function(coeur){
+            let top = parseInt(coeur.style.top.split("%")[0],10);
+            let left = parseInt(coeur.style.left.split("%")[0],10);
+            let min = -moveStep;
+            let max = moveStep;
+            let rdmTop = Math.floor(Math.random() * (max - min + 1)) + min;
+            let rdmLeft = Math.floor(Math.random() * (max - min + 1)) + min;
+
+            if (top + rdmTop > 100 - moveStep){
+                newTop = top - moveStep;
+            }else if (top - rdmTop < 0 + moveStep){
+                newTop = top + moveStep;
+            }else{
+                newTop = top + rdmTop;
+            }
+
+            if (left + rdmLeft > 100 - moveStep){
+                newLeft = left - moveStep;
+            }else if (left - rdmLeft < 0 + moveStep){
+                newLeft = left + moveStep;
+            }else{
+                newLeft = left + rdmLeft;
+            }
+
+            coeur.style.top = newTop+"%";
+            coeur.style.left = newLeft+"%";
+        });
+    }
+}
+
+function updateTimer(){
+    time--;
+    document.querySelector("#timer").firstElementChild.innerHTML = time;
+    document.title = "Timer : "+time;
 }
 
 function stopGame(){
+    clearInterval(timer);
+    clearInterval(timerPosition);
+    document.getElementById("timer").innerHTML = "0";
+    document.title = "Resultat : "+document.getElementById("score").innerHTML;
     let coeur = document.querySelectorAll(".coeur");
     coeur.forEach(function(c){
         c.removeEventListener("click",destroyCoeur);
     });
     document.querySelector("#score").style.color = "red";
+    clapclap.play();
 }
